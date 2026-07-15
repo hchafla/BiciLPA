50cffa3ee0359aef42eea616c9e9a767ffa1ce89
hchafla <161073227+hchafla@users.noreply.github.com>
2026-07-15 15:31:23 +0100

    Update manifest.js

--- datos/manifest.js
+++ datos/manifest.js
@@ -4,26 +4,30 @@
 // El archivo .js detallado solo se descargará cuando el usuario haga clic.
 // 
 // Valores válidos para el campo "eje":
-// - "central"
+// - "este"
 // - "norte"
 // - "baja-alta"
 // - "alta-baja"
 // =========================================================================
 const rutasManifest = {
   "santa-catalina-san-telmo": {
-    "titulo": "Santa Catalina a San Telmo",
-    "eje": "central"
+    "titulo": "Santa Catalina ⬌ San Telmo",
+    "eje": "este"
   },
   "castillo-mata-canodromo": {
-    "titulo": "Castillo de Mata a Canódromo",
+    "titulo": "Castillo de Mata ⮕ Canódromo",
     "eje": "baja-alta"
   },
   "canodromo-castillo-mata": {
-    "titulo": "Canódromo a Castillo de Mata",
+    "titulo": "Canódromo ⮕ Castillo de Mata",
     "eje": "alta-baja"
   },
+   "datos/rutas/las-arenas-hoya-de-la-plata": {
+    "titulo": "CC Las Arenas ⮕ Hoya de la Plata",
+    "eje": "este"
+  },
   "las-arenas-belen-maria": {
-    "titulo": "Las Arenas a Belén María",
+    "titulo": "CC Las Arenas ⮕ Belén María",
     "eje": "norte"
   }
 
@@ -33,7 +37,7 @@ const rutasManifest = {
   /*
   ,"id-de-tu-nueva-ruta": {
     "titulo": "Nombre Visible en el Botón",
-    "eje": "central"
+    "eje": "este"
   }
   */
 };
